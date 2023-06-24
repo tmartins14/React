e10f0e519b4e47fc9cae0b783d4486ee67a4f077
Tyler Martins <martinstyler14@gmail.com>
2023-06-24 18:40:38 -0400

    Printing Javascript variables

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// 1) Import react and reactdom libraries
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// 2) Get reference to the div with ID root
+const el = document.getElementById("root");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 3) Tell react to take control of that element
+const root = ReactDOM.createRoot(el);
+
+// 4) Create a component
+function App() {
+  let message = "Bye there!";
+
+  if (Math.random() > 0.5) {
+    message = "Hello there!";
+  }
+
+  return <div>{message}</div>;
+}
+//  5) Show the component on the screen
+root.render(<App />);
