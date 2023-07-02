8a80fc7efbc814a7fc4768809bc59d908a1b1037
Tyler Martins <martinstyler14@gmail.com>
2023-07-02 18:50:10 -0400

    Section 3: Building with Reusable Components pt.2

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,13 +1,17 @@
+import "bulma/css/bulma.css";
 import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import SiriImage from "./images/siri.png";
+import CortanaImage from "./images/cortana.png";
 
 function App() {
   return (
     <div>
       <div>Personal Digital Assistants</div>
 
-      <ProfileCard title="Alexa" handle="@alexa99" />
-      <ProfileCard title="Cortana" handle="@cortana32" />
-      <ProfileCard title="Siri" handle="@siri01" />
+      <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+      <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
+      <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
     </div>
   );
 }
