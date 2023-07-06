5d585c328789db4b9656adafb933f2d57eb8e005
Tyler Martins <martinstyler14@gmail.com>
2023-07-05 20:50:53 -0400

    Section 4: State: How to Change Your App - pt.3

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,16 +1,29 @@
 import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+
+import "./App.css";
+
+function getRandomAnimal() {
+  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+}
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [animals, setAnimals] = useState([]);
 
   const handleClick = () => {
-    setCount(count + 1);
+    setAnimals([...animals, getRandomAnimal()]);
   };
 
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index} />;
+  });
+
   return (
-    <div>
+    <div className="app">
       <button onClick={handleClick}>Add Animal</button>
-      <div>Number of Animals: {count}</div>
+      <div className="animal-list">{renderedAnimals}</div>
     </div>
   );
 }
