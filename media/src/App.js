dd3dfcabd01ca79993060911339732062fb44afc
Tyler Martins <martinstyler14@gmail.com>
2023-09-28 08:39:28 -0400

    Section 21: Interfacing with API's Using Async Thunks pt.1

---
+++ media/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import UsersList from "./components/UsersList";
+
+const App = () => {
+  return (
+    <div className="container mx-auto">
+      <UsersList />
+    </div>
+  );
+};
+
+export default App;
