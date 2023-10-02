a9aa127ad7801a336f5968d892e8617368c914a6
Tyler Martins <martinstyler14@gmail.com>
2023-10-02 12:03:19 -0400

    Section 21: Interfacing with API's Using Async Thunks pt.2

--- media/src/components/UsersList.js
+++ media/src/components/UsersList.js
@@ -1,7 +1,60 @@
 import React from "react";
+import UsersListItem from "./UsersListItem";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { fetchUsers, addUser } from "../store";
+import Button from "./Button";
+import Skeleton from "./Skeleton";
+import { useThunk } from "../hooks/useThunk";
 
 const UsersList = () => {
-  return <div>UsersList</div>;
+  const [doFetchUsers, isLoadingUsers, loadingUsersError] =
+    useThunk(fetchUsers);
+  const [doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser);
+
+  const { data } = useSelector((state) => {
+    return state.users;
+  });
+
+  useEffect(() => {
+    doFetchUsers();
+  }, [doFetchUsers]);
+
+  const handleUserAdd = () => {
+    doCreateUser();
+  };
+
+  let content;
+
+  if (isLoadingUsers) {
+    content = <Skeleton times={6} className="h-10 w-full" />;
+  } else if (loadingUsersError) {
+    content = <div>Error fetching data...</div>;
+  } else {
+    content = data.map((user) => {
+      return <UsersListItem key={user.id} user={user} />;
+      //   return (
+      //     <div key={user.id} className="mb-2 border rounded">
+      //       <div className="flex p-2 justify-between items-center cursor-pointer">
+      //         {user.name}
+      //       </div>
+      //     </div>
+      //   );
+    });
+  }
+
+  return (
+    <div>
+      <div className="flex flex-row justify-between m-3 items-center">
+        <h1 className="m-2 text-xl">Users</h1>
+        <Button loading={isCreatingUser} onClick={handleUserAdd}>
+          + Add User
+        </Button>
+        {creatingUserError}
+      </div>
+      {content}
+    </div>
+  );
 };
 
 export default UsersList;
