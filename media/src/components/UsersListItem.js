82a34adf5644a41f2ad145d3a3cdfeca7f5dce07
Tyler Martins <martinstyler14@gmail.com>
2023-10-03 18:45:19 -0400

    Section 22: Modern Async with Redux Toolkit Query pt. 1

--- media/src/components/UsersListItem.js
+++ media/src/components/UsersListItem.js
@@ -3,6 +3,8 @@ import { GoTrash } from "react-icons/go";
 import Button from "./Button";
 import { removeUser } from "../store";
 import { useThunk } from "../hooks/useThunk";
+import ExpandablePanel from "./ExpandablePanel";
+import AlbumsList from "./AlbumsList";
 
 const UsersListItem = ({ user }) => {
   const [doRemoveUser, isLoading, error] = useThunk(removeUser);
@@ -11,18 +13,21 @@ const UsersListItem = ({ user }) => {
     doRemoveUser(user);
   };
 
+  const header = (
+    <>
+      {" "}
+      <Button loading={isLoading} onClick={handleClick} className="mr-3">
+        <GoTrash />
+      </Button>
+      {error && <div>Error deleting user</div>}
+      {user.name}
+    </>
+  );
+
   return (
-    <div className="mb-2 border rounded">
-      <div className="flex p-2 justify-between items-center cursor-pointer">
-        <div className="flex flex-row items-center justify-between">
-          <Button loading={isLoading} onClick={handleClick} className="mr-3">
-            <GoTrash />
-          </Button>
-          {error && <div>Error deleting user</div>}
-          {user.name}
-        </div>
-      </div>
-    </div>
+    <ExpandablePanel header={header}>
+      <AlbumsList user={user} />
+    </ExpandablePanel>
   );
 };
 
