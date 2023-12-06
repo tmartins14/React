7a696c33223a483dedbe792ff89947791b807921
Tyler Martins <martinstyler14@gmail.com>
2023-12-06 08:54:46 -0500

    Added Animals App Screenshot

--- media/src/components/UsersList.js
+++ media/src/components/UsersList.js
@@ -33,13 +33,7 @@ const UsersList = () => {
   } else {
     content = data.map((user) => {
       return <UsersListItem key={user.id} user={user} />;
-      //   return (
-      //     <div key={user.id} className="mb-2 border rounded">
-      //       <div className="flex p-2 justify-between items-center cursor-pointer">
-      //         {user.name}
-      //       </div>
-      //     </div>
-      //   );
+
     });
   }
 
